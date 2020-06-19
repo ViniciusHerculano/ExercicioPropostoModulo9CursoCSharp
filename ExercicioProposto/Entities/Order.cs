4ac304ebc9b5a06598d970a38c41ec794d7a2de8
Vinicius Herculano <viniciusherculanomap@gmail.com>
2020-06-19 18:33:24 -0300

    Exercicio proposto finalizado

---
+++ ExercicioProposto/Entities/Order.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ExercicioProposto.Entities.Enums;
+using System.Globalization;
+
+namespace ExercicioProposto.Entities
+{
+    class Order
+    {
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+        public Client Client { get; set; }
+        public List<OrderItem> OrderItens { get; set; } = new List<OrderItem>();
+
+        public Order()
+        {
+        }
+
+        public Order(DateTime moment, OrderStatus status, Client client)
+        {
+            Moment = moment;
+            Status = status;
+            Client = client;
+        }
+
+        public void AddItem(OrderItem item)
+        {
+            OrderItens.Add(item);
+        }
+
+        public void RemoveItem(OrderItem item)
+        {
+            OrderItens.Remove(item);
+        }
+
+        public double Total()
+        {
+            double sum = 0.0;
+            foreach (OrderItem item in OrderItens)
+            {
+                sum += item.SubTotal();
+            }
+
+            return sum;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Order moment: ");
+            sb.AppendLine(Moment.ToString("dd/MM/yyyy HH:mm:ss"));
+            sb.Append("Order status:");
+            sb.AppendLine(Status.ToString());
+            sb.Append("Client: ");
+            sb.AppendLine(Client.ToString());
+            sb.Append("Order items: ");
+            foreach (var item in OrderItens)
+            {
+                sb.Append(item.Product.Name + ", ");
+                sb.Append("$" + item.Price + ", ");
+                sb.Append("Quantity: " + item.Quantity + ", ");
+                sb.AppendLine("Subtotal: " + item.SubTotal());
+
+
+            }
+            sb.Append("Total price: $");
+            sb.AppendLine(Total().ToString("F2", CultureInfo.InvariantCulture));
+            return sb.ToString();
+        }
+    }
+}
---
+++ ExercicioProposto/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using ExercicioProposto.Entities;
+using ExercicioProposto.Entities.Enums;
+using System.Globalization;
+
+namespace ExercicioProposto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+            Console.Write("Birth date (DD/MM/YYYY): ");
+            DateTime birthdate = DateTime.Parse(Console.ReadLine());
+
+            Client c1 = new Client(name, email, birthdate);
+
+            Console.WriteLine("Enter order data:");
+            Console.Write("Status: ");
+            OrderStatus status = Enum.Parse<OrderStatus>(Console.ReadLine());
+            Console.Write("How many items to this order? ");
+            int quantity = int.Parse(Console.ReadLine());
+            Order o1 = new Order(DateTime.Now, status, c1);
+
+            for (int i = 0; i < quantity; i++)
+            {
+                Console.WriteLine($"Enter #{i + 1} item data:");
+                Console.Write("Product name: ");
+                string prodname = Console.ReadLine();
+                Console.Write("Product price: ");
+                double prodprice = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Quantity: ");
+                int prodqtd = int.Parse(Console.ReadLine());
+
+                Product p1 = new Product(prodname, prodprice);
+
+                OrderItem orderitem = new OrderItem(prodqtd, prodprice, p1);
+                o1.AddItem(orderitem);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("ORDER SUMARY");
+            Console.WriteLine(o1);
+               
+
+
+        }
+    }
+}
